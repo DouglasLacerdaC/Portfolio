ee5d9b602a661f88793931323319f1fa55f54286
Douglas Lacerda <douglaslacerda544@gmail.com>
2022-02-23 23:36:56 -0300

    inicialização da navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolioV2';
+export class AppComponent implements OnInit {
+
+  @ViewChild('menu') menu: any
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  openMenu() {
+    this.menu.nativeElement.classList.toggle('menu-open')
+  }
+
 }
