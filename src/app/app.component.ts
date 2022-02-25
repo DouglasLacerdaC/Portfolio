05af36cbbc37d22d3795ce2c8f85b2924a8cbe65
Douglas Lacerda <douglaslacerda544@gmail.com>
2022-02-25 00:44:36 -0300

    criação do menu e criação da welcome e about-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,50 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class AppComponent implements OnInit {
 
   @ViewChild('menu') menu: any
+  @ViewChild('navMenu') navMenu: any
+  @ViewChildren('aside') aside: any
   
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    window.addEventListener('scroll', () => {
+      this.animaScroll()
+    })
+
+  }
 
   openMenu() {
+
     this.menu.nativeElement.classList.toggle('menu-open')
+    this.navMenu.nativeElement.classList.toggle('links-visible')
+
+    if (document.body.style.overflow == 'hidden')
+
+      document.body.style.overflow = 'auto'
+    
+    else document.body.style.overflow = 'hidden'
+
+  }
+
+  animaScroll() {
+
+    const topPage = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    
+    this.aside.forEach((element: any) => {
+
+      if(topPage > element.nativeElement.offsetTop) {
+
+        element.nativeElement.classList.add('aside-animation')
+  
+      } else {
+  
+        element.nativeElement.classList.remove('aside-animation')
+  
+      }
+      
+    })
+
   }
 
 }
