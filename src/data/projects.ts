13956dd9ed9f161642d26d12c53ed518bf18ba5a
Douglas Lacerda <douglaslacerda544@gmail.com>
2022-03-02 13:04:49 -0300

    inserção de links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,27 +4,27 @@ export const Projects = [
         description: `Projeto sendo realizado com afins de aprimorar estudos com front-end. O projeto tem a finalizade de ajudar as pessoas em organizar 
         suas tarefas diárias sendo assim, um bloco de notas web de fácil acesso.`,
         image: '../assets/img/projects/trellinho.svg',
-        link: ''
+        link: 'https://github.com/DouglasLacerdaC/trellinho'
     },
     {
         name: 'Social Network',
         description: `Com a finalidade de desenvolver uma rede social. Programei esse projeto onde 
         utilizei a api do Github para ter dados de usúarios e fazer login.`,
         image: '../assets/img/projects/social.gif',
-        link: ''
+        link: 'https://github.com/DouglasLacerdaC/SocialNetwork'
     },
     {
         name: 'Clone Spotify',
         description: `Projeto com principal finalidade de testar minhas habilidades e lógicas para desenvolver o 
         clone do spofify com HTML, CSS e Javascript.`,
         image: '../assets/img/projects/spotify.png',
-        link: ''
+        link: 'https://github.com/DouglasLacerdaC/SpotifyClone'
     },
     {
         name: 'Clone Twitch',
         description: `Outro projeto com finalidade de clonar uma grande plataforma, dessa vez a Twitch. 
         Busquei testar minhas habilidades com angular e colocando apenas conteúdos que consumo bastante.`,
         image: '../assets/img/projects/twitch.png',
-        link: 'https://'
+        link: 'https://github.com/DouglasLacerdaC/Twitch_Clone'
     }
 ]
